467828251282f352737e99bdd708cfa334fd8c81
Sirhexalot <adrian@schmolzi-online.de>
2026-02-02 15:04:08 +0100

    Small helper

---
+++ uuid.php
@@ -0,0 +1,30 @@
+
+<?php
+
+    //content
+    $content = file_get_contents('old.yaml');
+    $content = preg_replace_callback("|uuid: (.*)\n|U", function ($matches) { return "uuid: ".str_replace('-', '', generateUUIDv4())."\n"; }, $content);
+    
+    $fp = fopen("new.yaml", "w");
+    fwrite($fp, $content);
+    fclose($fp);
+        
+    
+    function generateUUIDv4() {
+        $data = random_bytes(16);
+
+        // Set version (4) and variant (2) bits
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+        // Convert binary data to hexadecimal format
+        $uuid = vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+
+        return $uuid;
+    }
+
+
+    echo $content
+    
+
+?>
